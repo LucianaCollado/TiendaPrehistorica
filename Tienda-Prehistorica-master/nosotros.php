3c5b5a98c338daba6a24eeda6a70de969d1a8ef0
Luciana Belén Collado <a_lucianacollado@hotmail.com>
2021-03-08 00:27:51 -0300

    Tercer commit

---
+++ Tienda-Prehistorica-master/nosotros.php
@@ -0,0 +1,18 @@
+<?php
+include "header.php"
+?>
+<div class="nosotros">
+    <div class="nosotros-menu">
+        <h3>Nuestra historia</h3>
+</div>
+    <div class="nosotros-info">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores exercitationem beatae ipsa cupiditate praesentium sapiente voluptatibus iste iusto sed deleniti impedit aperiam quibusdam, similique repellat, sint corrupti dolores nisi quos ea id esse. Illum, sit! Optio dolores placeat minus quam asperiores, dignissimos tenetur excepturi. Rem aut, quos laborum est veniam magnam maxime vero quod corporis id eos mollitia magni excepturi laudantium aspernatur totam quia voluptas distinctio provident natus tempore nisi impedit consectetur. Similique modi perspiciatis atque, dolore in nisi, dolorem dicta, accusantium accusamus velit culpa facilis? Similique rem perspiciatis, sed debitis velit consequatur incidunt cumque ducimus iste id qui fuga molestiae hic est nam repellendus distinctio consectetur officiis corrupti perferendis autem praesentium reiciendis non cum. Laboriosam consectetur optio necessitatibus amet voluptatem quasi, deserunt atque pariatur quia ipsum. Architecto repudiandae, ab commodi earum suscipit voluptate dolor error, aut asperiores rerum consequuntur ratione ad. Culpa iure laborum sint, ipsam dolore maiores ut exercitationem, enim tempora omnis ipsum, corrupti ad consequatur a alias autem delectus. Harum suscipit similique minima nemo eligendi iusto quia iste est vitae quis! Dolor, amet. Ipsum ea quae ad aliquam culpa veniam perferendis, voluptatem, nemo sapiente adipisci provident obcaecati eius, iure nesciunt nihil maiores nulla cumque est. Illo doloribus dicta ut facere dolorem voluptatem minima ex quo dolorum vitae quis magnam, facilis molestiae libero debitis officiis id cum? Quasi at quibusdam blanditiis. Culpa natus labore facere voluptatibus provident ipsum ullam rerum voluptate accusamus! Doloribus nulla asperiores rerum, quia, harum, unde quas id distinctio error laudantium animi minus qui similique doloremque deserunt quos. Repellendus obcaecati cum amet placeat, sapiente quam natus nam iusto animi minus saepe, totam illum? Soluta error ipsum sed cum eum perferendis earum nostrum commodi. Debitis illo ad incidunt aperiam, vel asperiores, reprehenderit enim dignissimos veritatis tempore doloremque voluptas. Maiores aliquam molestiae sapiente iste voluptas amet asperiores.
+
+        </p>
+
+    </div>
+</div>
+<?php
+include "footer.php"
+?>
